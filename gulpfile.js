34de5ae6892711b1032884be4cdc40b753c0e2e7
6669venus <31347458+6669venus@users.noreply.github.com>
2017-10-20 07:57:43 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
-var gulp = require('gulp'), connect = require('gulp-connect'), webserver = require('gulp-webserver');
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    webserver = require('gulp-webserver');
+
 gulp.task('default', function() {
   // place code for your default task here
 });
 
 gulp.task('connect', function() {
-  connect.server();
+  connect.server({
+    debug : true
+  });
 });
 
 gulp.task('webserver', function() {
@@ -20,3 +25,55 @@ gulp.task('webserver', function() {
 
 gulp.task('default', ['connect']);
 gulp.task('ws', ['webserver']);
+
+//-------------------------------------------------------------
+// more sample
+
+var morgan = require('morgan');
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+var browserify = require("browserify");
+var source = require('vinyl-source-stream');
+var tsify = require("tsify");
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+
+gulp.task("ts1", function () {
+    return tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("js"));
+});
+
+gulp.task("ts2", function () {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/main.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest("js"));
+});
+
+var logger = morgan(':method :status (:response-time ms) :url (size: :res[content-length])');
+gulp.task('ws2', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      port:8080,
+      host:"0.0.0.0",
+      livereload: false,
+      directoryListing: false,
+      open: false,
+      middleware: logger
+    }));
+});
+
